a386cee5d1198d819356d39639f9f8341a22b013
nboccolini <nico_bocco@live.com>
2017-12-12 12:27:31 -0300

    Fixed: seeds.rb

--- lib/generators/auth_m/templates/seeds/seeds.rb
+++ lib/generators/auth_m/templates/seeds/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AuthM::User.create(email:"root@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:root], active: true)
+AuthM::User.create(email:"root@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:root], active: true).save(validate: false)
 
 management_1 = AuthM::Management.create(name: "Empresa 1")
 management_2 = AuthM::Management.create(name: "Empresa 2")
@@ -17,8 +17,4 @@ admin_management_2 = management_2.users.create(email:"admin2@a.com", password:"a
 user_management_1 = management_1.users.create(email:"user1@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true)
 user_management_2 = management_1.users.create(email:"user2@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true)
 user_management_3 = management_2.users.create(email:"user3@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true)
-
-resource_1 = management_1.resources.create(name: "User")
-
-user_management_1.policies.create(resource_id: resource_1.id, access: "read")
 
