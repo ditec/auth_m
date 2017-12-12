ce072414295c0a51fdb9ca43c0386815ee64d890
nicoboccolini <nico_bocco@live.com>
2017-12-12 14:41:20 -0300

    Deleted: db folder

--- db/migrate/20171129145314_create_auth_m_managements.rb
+++
@@ -1,9 +0,0 @@
-class CreateAuthMManagements < ActiveRecord::Migration[5.1]
-  def change
-    create_table :auth_m_managements do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20171129150218_add_management_id_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddManagementIdToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :auth_m_users, :management
-
-    add_index(:auth_m_users, 'management_id', :name => 'index_managements_on_users')
-  end
-
-end
