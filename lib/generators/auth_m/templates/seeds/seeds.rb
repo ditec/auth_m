85667d501a148411b5907b203b845b9e587e76ea
nboccolini <nico_bocco@live.com>
2018-02-26 09:56:58 -0300

    Added: new seed

--- lib/generators/auth_m/templates/seeds/seeds.rb
+++ lib/generators/auth_m/templates/seeds/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AuthM::User.create(email:"root@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:root], active: true).save(validate: false)
+AuthM::User.create(email:"root@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:root], active: true, confirmed_at: DateTime.now.to_date).save(validate: false)
 
 management_1 = AuthM::Management.create(name: "Empresa 1")
 management_2 = AuthM::Management.create(name: "Empresa 2")
@@ -17,10 +17,10 @@ person3 = management_2.people.create(first_name:"Juan", last_name:"Pereira", dni
 person4 = management_2.people.create(first_name:"Lucas", last_name:"Negro", dni:"35332543" ) 
 person5 = management_2.people.create(first_name:"Maria", last_name:"Bueno", dni:"23323432" ) 
 
-admin_management_1 = person1.create_user(email:"admin1@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:admin], active: true)
-admin_management_2 = person3.create_user(email:"admin2@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:admin], active: true)
+admin_management_1 = person1.create_user(email:"admin1@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:admin], active: true, confirmed_at: DateTime.now.to_date )
+admin_management_2 = person3.create_user(email:"admin2@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:admin], active: true, confirmed_at: DateTime.now.to_date)
 
-user_management_1 = person2.create_user(email:"user1@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true)
-user_management_2 = person4.create_user(email:"user2@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true)
-user_management_3 = person5.create_user(email:"user3@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true)
+user_management_1 = person2.create_user(email:"user1@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true, confirmed_at: DateTime.now.to_date)
+user_management_2 = person4.create_user(email:"user2@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true, confirmed_at: DateTime.now.to_date)
+user_management_3 = person5.create_user(email:"user3@a.com", password:"asd12345", password_confirmation:"asd12345", roles: [:user], active: true, confirmed_at: DateTime.now.to_date)
 
